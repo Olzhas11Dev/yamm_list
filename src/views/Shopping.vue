5cb023e3dba5d8831551380708c7e570330119ca
Olzhas11Dev <olzhasdev11@gmail.com>
2021-12-02 04:07:14 +1300

    added menu bar

--- src/views/Shopping.vue
+++ src/views/Shopping.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="container">
     <h3 class="text-center mt-5 mb-5">Shopping</h3>
+    <div>Total:{{ rounded }}</div>
     <div class="row">
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-3" v-for="item in allItems" :key="item.id">
-        <div class="card">
-          <img :src="item.img" alt="" />
-          {{ item.title }}
+      <div class="col-9">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 mb-3" v-for="item in allItems" :key="item.id">
+            <div class="card item" @click="addToBasket(item)">
+              <img :src="item.img" alt="" />
+              {{ item.title }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-3 border pt-4">
+        <h3>Items</h3>
+        <div v-for="elem in addedToBasket" :key="elem.id">
+          <div class="bg-secondary d-flex justify-content-between mb-2 p-2 text-white">
+            <div>{{ elem.title }} x {{ elem.quantity }}</div>
+            <div>{{ (elem.quantity * elem.price).toFixed(2) }}</div>
+          </div>
         </div>
       </div>
     </div>
@@ -19,7 +33,24 @@ export default {
   data() {
     return {
       allItems: [],
+      addedToBasket: [],
+      total: 0,
+      rounded: 0,
     };
+  },
+
+  methods: {
+    addToBasket(product) {
+      let founded = this.addedToBasket.find((elem) => elem.id === product.id);
+      if (founded) {
+        founded.quantity += 1;
+        console.log(this.addedToBasket);
+      } else {
+        this.addedToBasket.push(product);
+      }
+      this.total += product.price;
+      this.rounded = Math.round((this.total + Number.EPSILON) * 100) / 100;
+    },
   },
 
   mounted() {
@@ -30,4 +61,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.item {
+  cursor: pointer;
+}
+</style>
